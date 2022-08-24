59d802a53b5d586027023dc19a769ed6afd9cdd2
Saiful182 <saifulislamcse0@gamil.com>
2022-08-24 22:40:19 +0600

    still working with symbol logics

--- functional.js
+++ functional.js
@@ -2,13 +2,38 @@ function buttonClick(buttonName) {
     const displayValue = document.getElementById('display').innerText;
     if (parseFloat(displayValue) === 0) {
         const sevenValue = document.getElementById(buttonName).innerText;
-        document.getElementById('display').innerText = sevenValue
+        document.getElementById('display').innerText = sevenValue;
     }
     else {
         const display = document.getElementById('display').innerText;
         const sevenValue = document.getElementById(buttonName).innerText;
         const newDisplay = display + sevenValue;
         document.getElementById('display').innerText = newDisplay;
+    }
+    return document.getElementById('display').innerText;
+}
+
+function getPriviosNumber() {
+    const priviousNumberString = document.getElementById('display').innerText;
+    const priviousNumbers = parseInt(priviousNumberString);
+    return priviousNumbers;
+}
+
+function symbolManipulation(symbol) {
+    if (symbol === 'plus') {
+        document.getElementById('display').innerText = '+';
+    }
+    else if (symbol === 'minus') {
+        document.getElementById('display').innerText = '-';
+    }
+    else if (symbol === 'multi') {
+        document.getElementById('display').innerText = 'X';
+    }
+    else if (symbol === 'division') {
+        document.getElementById('display').innerText = '/';
+    }
+    else {
+        document.getElementById('display').innerText = '%';
     }
     return document.getElementById('display').innerText;
 }
@@ -51,25 +76,29 @@ document.getElementById('e-braket').addEventListener('click', function () {
     const sevenValue = buttonClick('e-braket');
 });
 document.getElementById('percentage').addEventListener('click', function () {
-    const sevenValue = buttonClick('percentage');
+    const sevenValue = symbolManipulation('percentage');
 });
 document.getElementById('multi').addEventListener('click', function () {
-    const sevenValue = buttonClick('multi');
+    const sevenValue = symbolManipulation('multi');
 });
 document.getElementById('plus').addEventListener('click', function () {
-    const sevenValue = buttonClick('plus');
+    const sevenValue = symbolManipulation('plus');
 });
 document.getElementById('minus').addEventListener('click', function () {
-    const sevenValue = buttonClick('minus');
+    const sevenValue = symbolManipulation('minus');
 });
 document.getElementById('point').addEventListener('click', function () {
     const sevenValue = buttonClick('point');
 });
 document.getElementById('division').addEventListener('click', function () {
-    const sevenValue = buttonClick('division');
+    const sevenValue = symbolManipulation('division');
 });
 
 document.getElementById('equal').addEventListener('click', function () {
+    const displayValue = document.getElementById('display').innerText;
+    console.log(displayValue);
+    const convertedValue = parseInt(displayValue, 100);
+    console.log(convertedValue);
 
 });
 
@@ -77,5 +106,4 @@ document.getElementById('cancel').addEventListener('click', function () {
     const displayValue = document.getElementById('display').innerText;
     const newDisplay = displayValue.slice(0, -1);
     document.getElementById('display').innerText = newDisplay;
-
 });
